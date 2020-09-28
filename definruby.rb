0be382e7b104620881304229e74b820486fa780f
galvezn <72047040+galvezn@users.noreply.github.com>
2020-09-28 17:05:38 -0500

    Create definruby.rb

---
+++ definruby.rb
@@ -0,0 +1,16 @@
+def say_hello_ten_times
+  phrase = "Hello World"
+  print phrase
+  print phrase
+  print phrase
+  print phrase
+  print phrase
+  print phrase
+  print phrase
+  print phrase
+  print phrase
+  print phrase
+  end
+  
+  say_hello_ten_times
+  
